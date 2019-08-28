823cc32158115a0218d3f404a74d281262ed6d41
Elijah Boston <e.b.boston@gmail.com>
2019-08-28 15:57:02 -0400

    Update About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About Me</h1>
-    <p>I graduated in 2010 with a <i>Bachelors of Science in Computer Engineering from the University of South Florida</i>, with a minor in Physics.</p>
+    <p>I graduated in 2010 with a <b>Bachelors of Science in Computer Engineering from the University of South Florida</b>, and a minor in Physics.</p>
     <p>For the better part of a decade I've worked in various aspects of web development from frontend design, to backend API development, to managing infrastructure and automated processes.</p>
     <p>My interest in technology and web development has been lifelong. I made my first website at 13 on Geocities, taught myself Python during college, and setup my first server on an old Dell Optiplex around the same time.</p>
     <p>In my free time I enjoy travel, DJ'ing, gaming, and binging Netflix.</p>

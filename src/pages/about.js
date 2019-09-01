cd3476f85551e0b3d4d87bba4dad82c52030ac67
Elijah Boston <e.b.boston@gmail.com>
2019-09-01 01:00:27 -0400

    Remove mark tags for now

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About Me</h1>
-    <p>For the better part of a decade I've worked in various aspects of web development ranging from <mark>frontend design</mark>, <mark>backend API development</mark>, to <mark>managing servers</mark> and <mark>automating processes</mark>.</p>
+    <p>For the better part of a decade I've worked in various aspects of web development ranging from frontend design, backend API development, to managing servers and automating processes.</p>
     <p>My interest in technology and web development has been lifelong. I made my first website at 13 on Geocities, started teaching myself Python in college, and setup my first server on an old Dell Optiplex not long after that.</p>
     <p>I have a degree in Computer Engineering from the University of South Florida, and a minor in Physics.</p>
   </Layout>
